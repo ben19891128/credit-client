e1a3082975c14c0f0d93271e1dc53490838b5660
ben.ma <>
2018-10-26 14:34:34 +0800

    🐒

--- README.md
+++ README.md
@@ -17,15 +17,16 @@ CreditClient creditClient = new CreditClient.Builder()
 调用接口
 
 ```java
- MirrorFraud001ReqVo reqVo = new MirrorFraud001ReqVo();
- reqVo.setIdNo("341225198906274952");
- reqVo.setName("马化腾");
- reqVo.setOrgName("腾通讯");
- reqVo.setMobile("15618579663");
- String uri = "/api/v1/mirror/anti-fraud/001";
- HashMap<String, String> queryMap = Maps.newHashMap();
- ResponseVo responseVo = creditClient.executeJson(uri, "POST",JsonUtils.toJson(reqVo),     queryMap,Maps.newHashMap());
- System.out.println(responseVo);
+   MirrorFraud001ReqVo reqVo = new MirrorFraud001ReqVo();
+    reqVo.setIdNo("341225198906274952");
+    reqVo.setName("马化腾");
+    reqVo.setOrgName("腾通讯");
+    reqVo.setMobile("19015699662");
+    String uri = "/api/v1/mirror/anti-fraud/001";
+    HashMap<String, String> queryMap = Maps.newHashMap();
+    ResponseVo responseVo = creditClient
+        .executeJson(uri, "POST", JsonUtils.toJson(reqVo), queryMap, Maps.newHashMap());
+    System.out.println(responseVo);
 ```
 
 
--- credit-client-java/src/test/java/com/csci/cloud/client/test/CloudMirrorClientTest.java
+++ credit-client-java/src/test/java/com/csci/cloud/client/test/CloudMirrorClientTest.java
@@ -12,20 +12,21 @@ import org.junit.Test;
  * 个人反欺诈业务.
  */
 public class CloudMirrorClientTest extends BaseClientTest {
-    private static String HOST = "http://localhost:8020";
+
+  private static String HOST = "http://localhost:8020";
 
 
-    @Test
-    public void fraud001() throws Exception {
-        MirrorFraud001ReqVo reqVo = new MirrorFraud001ReqVo();
-
-        reqVo.setIdNo("341225198906274952");
-        reqVo.setName("马化腾");
-        reqVo.setOrgName("腾通讯");
-        reqVo.setMobile("15618579663");
-        String uri = "/api/v1/mirror/anti-fraud/001";
-        HashMap<String, String> queryMap = Maps.newHashMap();
-        ResponseVo responseVo = creditClient.executeJson(uri, "POST",JsonUtils.toJson(reqVo), queryMap,Maps.newHashMap());
-        System.out.println(responseVo);
-    }
+  @Test
+  public void fraud001() throws Exception {
+    MirrorFraud001ReqVo reqVo = new MirrorFraud001ReqVo();
+    reqVo.setIdNo("341225198906274952");
+    reqVo.setName("马化腾");
+    reqVo.setOrgName("腾通讯");
+    reqVo.setMobile("19015699662");
+    String uri = "/api/v1/mirror/anti-fraud/001";
+    HashMap<String, String> queryMap = Maps.newHashMap();
+    ResponseVo responseVo = creditClient
+        .executeJson(uri, "POST", JsonUtils.toJson(reqVo), queryMap, Maps.newHashMap());
+    System.out.println(responseVo);
+  }
 }
