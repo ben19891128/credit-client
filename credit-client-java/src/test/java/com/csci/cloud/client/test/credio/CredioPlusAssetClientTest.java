264754f649b4426c408ac3541d156ec301516af4
ben.ma <>
2018-11-19 15:04:15 +0800

    添加区块链测试接口

--- credit-client-java/src/test/java/com/csci/cloud/client/test/credio/CredioPlusAssetClientTest.java
+++ credit-client-java/src/test/java/com/csci/cloud/client/test/credio/CredioPlusAssetClientTest.java
@@ -5,6 +5,7 @@ import com.csci.cloud.client.common.JsonUtils;
 import com.csci.cloud.client.model.ResponseVo;
 import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.Maps;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
@@ -33,6 +34,32 @@ public class CredioPlusAssetClientTest extends AbstractCredioPushTest {
             JsonUtils.toJson(new Builder<>().put("assetId",assetId).put("status","05").build()),
             Maps.newHashMap(),
             defaultHeaderMap);
+        System.out.println(responseVo);
+    }
+
+
+    /**
+     * 查询上链资产.
+     * @throws Exception
+     */
+    @Test
+    public void queryAssetInfoById() throws Exception {
+        String uri = "/chain/api/data/asset/queryAssetInfoById/INV0001";
+        Map queryMap = Maps.newHashMap();
+        ResponseVo responseVo = creditClient.executeForm(uri,"GET",null,queryMap, initLoginMap());
+        System.out.println(responseVo);
+    }
+
+
+    /**
+     * 查询上链交易易信息.
+     * @throws Exception
+     */
+    @Test
+    public void queryTransInfoById() throws Exception {
+        String uri = "/chain/api/data/asset/queryTransInfoById/123213";
+        Map queryMap = Maps.newHashMap();
+        ResponseVo responseVo = creditClient.executeForm(uri,"GET",null,queryMap, initLoginMap());
         System.out.println(responseVo);
     }
 
--- credit-client-java/src/test/java/com/csci/cloud/client/test/credio/CredioPushAssetQueryClientTest.java
+++ credit-client-java/src/test/java/com/csci/cloud/client/test/credio/CredioPushAssetQueryClientTest.java
@@ -1,17 +1,17 @@
 package com.csci.cloud.client.test.credio;
 
-import com.csci.cloud.client.common.JsonUtils;
 import com.csci.cloud.client.model.ResponseVo;
-import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.Maps;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 public class CredioPushAssetQueryClientTest extends AbstractCredioPushTest {
 
 
+  /**
+   *  交易易最新状态查询
+   * @throws Exception
+   */
   @Test
   public void queryAsset() throws Exception {
     String assetId = "RQoPSZUYQHjDYbWXfxYggIzbXHkWopOt";
@@ -19,6 +19,25 @@ public class CredioPushAssetQueryClientTest extends AbstractCredioPushTest {
     Map<String, String> paraMap = Maps.newHashMap();
     paraMap.put("assetId",assetId);
     paraMap.put("productName","1234");
+    ResponseVo responseVo = creditClient.executeJson(uri,
+        "GET",
+        null,
+        paraMap,
+        initLoginMap());
+    System.out.println(responseVo);
+  }
+
+  /**
+   *  资产流转记录查询(由交易易往前追溯)
+   * @throws Exception
+   */
+  @Test
+  public void findTransFlowByTransId() throws Exception {
+    String transId = "123213";
+    String uri = "/chain/api/es-query/client/assetOnChain/findTransFlowByTransId";
+    Map<String, String> paraMap = Maps.newHashMap();
+    paraMap.put("transId",transId);
+    paraMap.put("productName","ASSET_2_BLOCKCHAIN");
     ResponseVo responseVo = creditClient.executeJson(uri,
         "GET",
         null,
